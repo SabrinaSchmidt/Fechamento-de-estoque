bce02d89f94a5ec9194a029afa6123390b662d62
Sabrina <sabrinaschmidt335@gmail.com>
2021-07-04 20:11:24 -0300

    Primeiras modificações

---
+++ Mainproject.dpr
@@ -0,0 +1,14 @@
+program Mainproject;
+
+uses
+  Vcl.Forms,
+  MainView in 'MainView.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
